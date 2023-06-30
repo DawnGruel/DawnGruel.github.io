64b74592f186f46ca0e40862ed01b081efcc0a3a
DawnGruel <247328581@qq.com>
2023-06-30 16:29:25 +0800

    40r

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:<dawngruel>/<dawngruel>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<dawngruel>/<REPO>.git master:gh-pages
 
 cd -
