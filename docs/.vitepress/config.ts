47f03528498ce70c6fc00c10d0758b6ded1c85b3
DawnGruel <247328581@qq.com>
2023-06-30 15:44:25 +0800

    404

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '../blog',
+  base: './',
   // base: '/',
   outDir: './blog/',
   title: "曦粥的博客",
