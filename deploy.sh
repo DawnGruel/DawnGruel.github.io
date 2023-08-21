01410c8112082c0d19db5798e89477e9c0572899
DawnGruel <247328581@qq.com>
2023-08-21 17:29:29 +0800

    Default commit message

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,34 @@
 # 忽略错误
 set -e
 
-# 构建
-echo "开始构建..."
-npm run docs:build
+build() {
+    # 构建
+    echo "开始构建..."
+    npm run docs:build
 
-# 进入待发布的目录
-# echo "进入待发布的目录..."
-# cd docs/blog
+    echo "开始构建Git提交..."
+    git add .
+    git commit -m "$commit_message"
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-echo "开始构建Git提交..."
-git add .
-git commit -m "new file"
+    # 如果部署到 https://<USERNAME>.github.io
+    echo "推送到远程仓库..."
+    git push -f https://github.com/DawnGruel/DawnGruel.github.io.git $branch
 
-# 如果部署到 https://<USERNAME>.github.io
-echo "推送到远程仓库..."
-git push -f https://github.com/DawnGruel/DawnGruel.github.io.git dev
+    echo "推送完成."
+    cd -
+}
 
-# 如果是部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<dawngruel>/<REPO>.git master:gh-pages
-echo "推送完成."
-cd -
+# 设置参数的默认值
+command="${1:-build}"
+commit_message="${2:-Default commit message}"
+branch="${3:-dev}"
+
+case "$command" in
+    "build")
+        build
+        ;;
+    *)
+        echo "Usage: $0 [build] [commit_message] [branch]"
+        exit 1
+        ;;
+esac
